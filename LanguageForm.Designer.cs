111e97fb9a86f667158155d7b541014227e39621
Sigmanor <sigmanor@users.noreply.github.com>
2015-07-23 21:02:24 +0300

    Added "Change Language" form
    Added form for changing launcher language, that opens on the first start

---
+++ LanguageForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Aion_Launcher
+{
+    partial class LanguageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LanguageForm));
+            this.langComboBox = new System.Windows.Forms.ComboBox();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // langComboBox
+            // 
+            this.langComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.langComboBox.FormattingEnabled = true;
+            this.langComboBox.Location = new System.Drawing.Point(12, 12);
+            this.langComboBox.Name = "langComboBox";
+            this.langComboBox.Size = new System.Drawing.Size(136, 21);
+            this.langComboBox.TabIndex = 1;
+            // 
+            // nextButton
+            // 
+            this.nextButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.nextButton.Location = new System.Drawing.Point(97, 41);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(51, 23);
+            this.nextButton.TabIndex = 2;
+            this.nextButton.Text = "Next";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LanguageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(159, 71);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.langComboBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LanguageForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Launcher Language";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.LanguageForm_FormClosing);
+            this.Load += new System.EventHandler(this.Language_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox langComboBox;
+        private System.Windows.Forms.Button nextButton;
+    }
+}
---
+++ LanguageForm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Aion_Launcher
+{
+    public partial class LanguageForm : Form
+    {
+        Properties.Settings ps = Properties.Settings.Default;
+
+        public LanguageForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Language_Load(object sender, EventArgs e)
+        {
+            CultureInfo ci = CultureInfo.InstalledUICulture;
+            if (ci.Name == "ru-RU")
+            {
+                this.Text = "Язык Лаунчера";
+                nextButton.Text = "Далее";
+            }
+
+            langComboBox.DataSource = new CultureInfo[]{
+            CultureInfo.GetCultureInfo("ru-RU"),
+            CultureInfo.GetCultureInfo("en-US")
+                  };
+
+            langComboBox.DisplayMember = "NativeName";
+            langComboBox.ValueMember = "Name";
+
+            if (!String.IsNullOrEmpty(ps.Language) && ci.Name != "ru-RU")
+            {
+                langComboBox.SelectedValue = ps.Language;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (langComboBox.Text == ("русский (Россия)") & !Directory.Exists("ru-RU"))
+            {
+                Directory.CreateDirectory("ru-RU");
+                File.WriteAllBytes(@".\ru-RU\Aion Game Launcher.resources.dll", Properties.Resources.ru);
+            }
+
+            ps.LangCheck = true;
+            ps.Language = langComboBox.SelectedValue.ToString();
+            ps.Save();
+            CultureInfo cultureInfo = new CultureInfo(ps.Language);
+            ChangeLanguage.Instance.localizeForm(this, cultureInfo);
+            this.Close();
+        }
+
+        private void LanguageForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (ps.LangCheck == false)
+            {
+                e.Cancel = true;
+            }
+            if (ps.LangCheck == true)
+            {
+                e.Cancel = false;
+            }
+        }   
+
+    }
+}
