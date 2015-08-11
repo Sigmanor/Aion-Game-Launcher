0bf89b24158a53736fcf2c0dbd2686749e96dceb
Sigmanor <sigmanor@users.noreply.github.com>
2015-08-11 18:31:57 +0300

    Initial commit

---
+++ Updater/UpdateForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Updater
+{
+    partial class UpdateForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UpdateForm));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.MarqueeAnimationSpeed = 1;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.UseWaitCursor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Updater.Properties.Resources.ajax_loader_bar;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.UseWaitCursor = true;
+            // 
+            // UpdateForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "UpdateForm";
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Updater/UpdateForm.cs
@@ -0,0 +1,77 @@
+﻿using Ionic.Zip;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace Updater
+{
+
+    public partial class UpdateForm : Form
+    {
+        string[] arg;
+
+        public UpdateForm(string[] args)
+        {
+            arg = args;
+            InitializeComponent();
+        }
+
+
+        #region ExtractZip
+        public void ExtractFileToDirectory(string zipFileName, string outputDirectory)
+        {
+            ZipFile zip = ZipFile.Read(zipFileName);
+            Directory.CreateDirectory(outputDirectory);
+            zip.ExtractAll(outputDirectory, ExtractExistingFileAction.OverwriteSilently);
+        }
+        #endregion
+
+
+        void downloader_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            var pr = new Process();
+
+            if (e.Error != null)
+            {
+                pr.StartInfo.FileName = "Aion Game Launcher.exe";
+                pr.StartInfo.Arguments = "/e";
+                pr.Start();
+            }
+
+            if (e.Error == null)
+            {
+                ExtractFileToDirectory("Aion-Game-Launcher-Update.zip", @".\");
+
+                pr.StartInfo.FileName = "Aion Game Launcher.exe";
+                pr.StartInfo.Arguments = "/u";
+                pr.Start();
+
+            }
+
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (arg.Length == 0)
+            {
+                this.Close();
+            }
+
+            else if (arg[0] == "/u")
+            {
+                WebClient webClient = new WebClient();
+
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(downloader_DownloadFileCompleted);
+
+                webClient.DownloadFileAsync(new Uri("http://sigmanor.tk/soft/Aion-Game-Launcher/Update.zip"), "Aion-Game-Launcher-Update.zip");
+            }
+        }
+
+
+
+    }
+}
