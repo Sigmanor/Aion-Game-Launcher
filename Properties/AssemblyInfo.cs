8c26a565faecbc86124cce2dd4e2a9a8589361f4
Sigmanor <sigmanor@users.noreply.github.com>
2015-08-20 15:05:44 +0300

    Version change

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 //[assembly: AssemblyFileVersion("2.4.667.0")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.6.0.0")]
+[assembly: AssemblyFileVersion("2.6.1.0")]
